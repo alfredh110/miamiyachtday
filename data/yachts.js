afc95187a50af45b72752a0867cf4c5bfbf3ed74
alfredh110 <HILLERCAPITALINC@GMAIL.COM>
2025-07-05 15:26:22 -0400

    Update yachts.js

--- data/yachts.js
+++ data/yachts.js
@@ -1,1 +1,19 @@
+const yachts = [
+  {
+    name: "Sundeck 26' Sport Boat",
+    desc: "26ft • 10 Guests • Crew Included",
+    img: "/AB-MODELIT-BOAT-MIAMIYACHTDAY-18.JPG"
+  },
+  {
+    name: "30' HURRICANE GRANDSON",
+    desc: "30ft • 10 Guests • Crew Included",
+    img: "/AB-30FT HURRICANE GRANDSON.jpg"
+  },
+  {
+    name: "31' BLACK PEAR AMBERJACK",
+    desc: "31ft • 13 Guests • 1 Cabin",
+    img: "/AB-31FT BLACK PEAR AMBERJACK.jpg"
+  }
+];
 
+export default yachts;
