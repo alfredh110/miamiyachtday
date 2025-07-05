9700fd751d7ac44b1c50bbe138966ff0c9256a57
alfredh110 <HILLERCAPITALINC@GMAIL.COM>
2025-07-05 15:30:03 -0400

    Update Footer.jsx

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -1,1 +1,22 @@
+import React from "react";
 
+export default function Footer() {
+  return (
+    <footer style={{
+      background: "#23304B",
+      color: "#FFD700",
+      padding: "2rem 0",
+      marginTop: "3rem",
+      textAlign: "center"
+    }}>
+      <div style={{ fontFamily: "Montserrat, sans-serif", fontSize: 22 }}>
+        © Miami Yacht Day
+      </div>
+      <div style={{ marginTop: 12 }}>
+        <span style={{ margin: "0 18px" }}>IG</span>
+        <span style={{ margin: "0 18px" }}>FB</span>
+        <span style={{ margin: "0 18px" }}>X</span>
+      </div>
+    </footer>
+  );
+}
