42be830feb97384a32daaf813f41952cc34a5bce
alfredh110 <HILLERCAPITALINC@GMAIL.COM>
2025-07-05 09:44:01 -0400

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,17 +1,16 @@
-import { SessionProvider } from "next-auth/react";
+import "./globals.css";
+import type { Metadata } from "next";
 
-export const metadata = {
-  title: 'Miami Yacht Day',
-  description: 'Your next adventure starts here!',
+export const metadata: Metadata = {
+  title: "Miami Yacht Day",
+  description: "Your next adventure starts here!",
 };
 
-export default function RootLayout({ children }) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>
-        <SessionProvider>
-          {children}
-        </SessionProvider>
+        {children}
       </body>
     </html>
   );
