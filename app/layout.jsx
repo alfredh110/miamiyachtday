5e2e270993476786f52e863925506d1ba99b005f
alfredh110 <HILLERCAPITALINC@GMAIL.COM>
2025-07-08 12:04:20 -0400

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,7 +1,9 @@
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body style={{ margin: 0, padding: 0 }}>
+        {children}
+      </body>
     </html>
   );
 }
